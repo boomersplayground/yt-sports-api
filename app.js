b7bf510b1730bec4705476faca78276549f62d16
Boomer <boomersplayground@gmail.com>
2025-01-08 11:56:55 -0700

    commiting after video 007 sorry :(

---
+++ app.js
@@ -0,0 +1,30 @@
+const url = `https://calm-chimera-0ce8b2.netlify.app/.netlify/functions/get_teams`
+const buttons = document.querySelectorAll('.button') 
+const container = document.querySelector('.container')
+
+const drawTeams = (teams) => {
+  console.log("t ", teams)
+  buttons.forEach(button => {
+    button.remove()
+  })
+
+  teams.teams.teams.forEach(team => {
+    const button = document.createElement("button")
+    button.classList.add("team", team.code)
+    button.innerText = team.name
+    container.append(button)
+  })
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', async () => {
+    const { sport } = button.dataset
+    try {
+      const data = await fetch(url + `?sport=${sport}`) 
+      const teams = await data.json()
+      drawTeams(teams)
+    } catch(error) {
+      console.error('err ', error)
+    }
+  })
+})
